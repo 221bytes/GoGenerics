d7610fd41d0170ff117ac77d1165b179fde63d56
221Bytes <catalanoalexandre@gmail.com>
2016-12-14 20:11:02 +0100

    More logical test arch

--- generics/algo.go
+++ generics/algo.go
@@ -5,35 +5,6 @@ import (
 	"unsafe"
 )
 
-type Test struct {
-	s string
-	a int
-}
-
-var a []Test
-var s []string
-
-func init() {
-	a = generateArrayStruct()
-	s = generateArrayString()
-}
-
-func generateArrayStruct() []Test {
-	a := make([]Test, 1000000)
-	for i := 0; i < 1000000; i++ {
-		a[i] = Test{s: string(i)}
-	}
-	return a
-}
-
-func generateArrayString() []string {
-	a := make([]string, 1000000)
-	for i := 0; i < 1000000; i++ {
-		a[i] = string(i)
-	}
-	return a
-}
-
 func ReverseGeneric(start unsafe.Pointer, size uintptr, lenght int) {
 	last := uintptr(start) + size*uintptr(lenght)
 	for i := lenght/2 - 1; i >= 0; i-- {
@@ -61,48 +32,4 @@ func DoReverse(a interface{}) {
 	length := reflect.ValueOf(a).Len()
 
 	ReverseGeneric(unsafe.Pointer(start), size, length)
-}
-
-func testGenericFastStruct() {
-	a := generateArrayStruct()
-	start := unsafe.Pointer(&a[0])
-	size := unsafe.Sizeof(a[0])
-	lenght := len(a)
-	ReverseGeneric(start, size, lenght)
-}
-
-func testGenericSlowStruct() {
-	a := generateArrayStruct()
-	DoReverse(a)
-}
-
-func testStruct() {
-	a := generateArrayStruct()
-	length := len(a)
-	for i := length/2 - 1; i >= 0; i-- {
-		opp := length - 1 - i
-		a[i], a[opp] = a[opp], a[i]
-	}
-}
-
-func testString() {
-	a := []string{"0", "1", "2", "3", "4"}
-	length := len(a)
-	for i := length/2 - 1; i >= 0; i-- {
-		opp := length - 1 - i
-		a[i], a[opp] = a[opp], a[i]
-	}
-}
-
-func testGenericFastString() {
-	a := []string{"0", "1", "2", "3", "4"}
-	start := unsafe.Pointer(&a[0])
-	size := unsafe.Sizeof(a[0])
-	lenght := len(a)
-	ReverseGeneric(start, size, lenght)
-}
-
-func testGenericSlowString() {
-	a := []string{"0", "1", "2", "3", "4"}
-	DoReverse(a)
 }
--- generics/algo_test.go
+++ generics/algo_test.go
@@ -1,6 +1,39 @@
-package generics
+package generics_test
 
-import "testing"
+import (
+	"testing"
+	"unsafe"
+
+	"github.com/221bytes/generics/generics"
+)
+
+type Test struct {
+	s string
+	a int
+}
+
+var a []Test
+var s []string
+
+func init() {
+	a = generateArrayStruct()
+	s = generateArrayString()
+}
+
+func generateArrayStruct() []Test {
+	a := make([]Test, 1000000)
+	for i := 0; i < 1000000; i++ {
+		a[i] = Test{s: string(i)}
+	}
+	return a
+}
+func generateArrayString() []string {
+	a := make([]string, 1000000)
+	for i := 0; i < 1000000; i++ {
+		a[i] = string(i)
+	}
+	return a
+}
 
 func BenchmarkGenericFastStruct(b *testing.B) {
 	for n := 0; n < b.N; n++ {
@@ -36,4 +69,48 @@ func BenchmarkString(b *testing.B) {
 	for n := 0; n < b.N; n++ {
 		testString()
 	}
+}
+
+func testGenericFastStruct() {
+	a := generateArrayStruct()
+	start := unsafe.Pointer(&a[0])
+	size := unsafe.Sizeof(a[0])
+	lenght := len(a)
+	generics.ReverseGeneric(start, size, lenght)
+}
+
+func testGenericSlowStruct() {
+	a := generateArrayStruct()
+	generics.DoReverse(a)
+}
+
+func testStruct() {
+	a := generateArrayStruct()
+	length := len(a)
+	for i := length/2 - 1; i >= 0; i-- {
+		opp := length - 1 - i
+		a[i], a[opp] = a[opp], a[i]
+	}
+}
+
+func testString() {
+	a := []string{"0", "1", "2", "3", "4"}
+	length := len(a)
+	for i := length/2 - 1; i >= 0; i-- {
+		opp := length - 1 - i
+		a[i], a[opp] = a[opp], a[i]
+	}
+}
+
+func testGenericFastString() {
+	a := []string{"0", "1", "2", "3", "4"}
+	start := unsafe.Pointer(&a[0])
+	size := unsafe.Sizeof(a[0])
+	lenght := len(a)
+	generics.ReverseGeneric(start, size, lenght)
+}
+
+func testGenericSlowString() {
+	a := []string{"0", "1", "2", "3", "4"}
+	generics.DoReverse(a)
 }
